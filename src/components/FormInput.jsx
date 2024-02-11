1baf96a2b571c101f0c512fd511d8f22a1e3085b
miawithcode <cuttontail@gmail.com>
2024-02-11 18:34:51 +0800

    feat: login page

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -2,7 +2,7 @@ const FormInput = ({ label, name, type }) => {
   return (
     <div className="form-control">
       <div className="label">
-        <span className="label-text">{label}</span>
+        <span className="label-text capitalize">{label}</span>
       </div>
       <input type={type} name={name} className="input input-bordered" />
     </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,35 @@
+import { Form, Link } from 'react-router-dom';
+import { FormInput, SubmitBtn } from '../components';
+
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
-export default Login
+    <section className=" h-screen grid place-items-center">
+      <Form
+        method="POST"
+        className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
+      >
+        <h4 className="text-center text-3xl capitalize font-bold">login</h4>
+        <FormInput label="email" name="identifier" type="email" />
+        <FormInput label="password" name="password" type="password" />
+
+        <div className="mt-4">
+          <SubmitBtn text="login" />
+        </div>
+
+        <button type="button" className="btn btn-secondary btn-block">
+          guest user
+        </button>
+        <p className="text-center">
+          Not a member yet?
+          <Link
+            to="/register"
+            className="ml-2 link link-hover link-primary capitalize"
+          >
+            register
+          </Link>
+        </p>
+      </Form>
+    </section>
+  );
+};
+export default Login;
