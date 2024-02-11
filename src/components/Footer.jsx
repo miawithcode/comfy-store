c33f3cb26ba97530df785432ab65265b1bd3366c
miawithcode <cuttontail@gmail.com>
2024-02-11 23:32:14 +0800

    feat: about page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content absolute bottom-0">
       <aside>
         <p>Copyright © {new Date().getFullYear()} - Made with 🩷 by Mia</p>
       </aside>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,28 @@
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
-export default About
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 justify-center items-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest capitalize">
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Kickstarter umami succulents hoodie. Lomo ethical photo booth raw denim
+        bespoke roof party la croix palo santo yuccie pop-up shabby chic
+        portland squid poke. Taxidermy aesthetic thundercats, adaptogen tousled
+        gastropub blog sustainable synth lo-fi pop-up fingerstache fixie occupy.
+        IPhone af etsy, ascot meh adaptogen dreamcatcher plaid tilde kale chips
+        organic mukbang. Raw denim salvia neutral milk hotel selvage yr shabby
+        chic. Gorpcore glossier gochujang butcher ramps thundercats.
+      </p>
+    </>
+  );
+};
+export default About;
