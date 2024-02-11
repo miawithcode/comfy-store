c8ad100798b45ed633c265953276393e5e67caf2
miawithcode <cuttontail@gmail.com>
2024-02-12 02:49:45 +0800

    feat: redesign featured products

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,10 +1,19 @@
+import { Link } from 'react-router-dom';
 import ProductsGrid from './ProductsGrid';
 import SectionTitle from './SectionTitle';
 
 const FeaturedProducts = () => {
   return (
     <div className="pt-24">
-      <SectionTitle title="featured products" desc="We offer best products" />
+      <div className="flex justify-between items-center">
+        <SectionTitle title="featured products" desc="We offer best products" />
+        <Link
+          to="products"
+          className="btn btn-secondary text-base-100 tracking-wide capitalize self-end"
+        >
+          View All Products
+        </Link>
+      </div>
       <ProductsGrid />
     </div>
   );
--- src/components/ProductsGrid.jsx
+++ src/components/ProductsGrid.jsx
@@ -17,21 +17,21 @@ const ProductsGrid = () => {
             className="card w-full shadow-xl hover:shadow-2xl transition duration-300"
           >
             <figure className="px-4 pt-4">
-              <div className="relative w-full">
-                <img
-                  src={image}
-                  alt={title}
-                  className="rounded-xl h-64 md:h-48 w-full object-cover"
-                />
-                <div className="absolute top-0 left-0 badge-md badge-ghost p-4 flex justify-center items-center rounded-tl-xl rounded-br-xl">
-                  {company}
-                </div>
-              </div>
+              <img
+                src={image}
+                alt={title}
+                className="rounded-xl h-64 md:h-48 w-full object-cover"
+              />
             </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title capitalize tracking-wider">{title}</h2>
-              <div className="text-secondary">{dollarsAmount}</div>
-              <div className="badge badge-secondary text-base-100 p-3">
+            <div className="card-body items-center">
+              <div className="w-full flex items-center justify-between">
+                <h2 className="card-title capitalize tracking-wider">
+                  {title}
+                </h2>
+                <div className="text-secondary">{dollarsAmount}</div>
+              </div>
+              <p className="text-gray-500 self-start">{company}</p>
+              <div className="badge badge-neutral text-base-100 p-3 self-start">
                 {category}
               </div>
             </div>
