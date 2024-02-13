a1d10f1f729cf88451cde2a78a41d75bb870ef04
miawithcode <cuttontail@gmail.com>
2024-02-14 01:23:18 +0800

    feat: filters params

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,9 +4,19 @@ import { customFetch } from '../utils';
 const url = '/products';
 
 export const loader = async ({ request }) => {
-  const response = await customFetch(url);
-  return { products: response.data.data, meta: response.data.meta };
-  
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+
+  // ALTERNATIVE - MANUALLY GET EACH VALUE:
+  // const params = new URL(request.url).searchParams;
+  // const search = params.get('search'); // Get Search Value
+
+  const response = await customFetch(url, {
+    params: params,
+  });
+  return { products: response.data.data, meta: response.data.meta, params };
+
   // ALTERNATIVE:
   // const products = response.data.data;
   // const meta = response.data.meta;
