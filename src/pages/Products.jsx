4da26c38fef00ce310ac20299ed0ca835b9f8d7d
miawithcode <cuttontail@gmail.com>
2024-02-13 19:16:30 +0800

    feat: setup products page and product loader

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,25 @@
+import { Filters, PaginationContainer, ProductsContainer } from '../components';
+import { customFetch } from '../utils';
+
+const url = '/products';
+
+export const loader = async ({ request }) => {
+  const response = await customFetch(url);
+  return { products: response.data.data, meta: response.data.meta };
+  
+  // ALTERNATIVE:
+  // const products = response.data.data;
+  // const meta = response.data.meta;
+  // return { products, meta };
+};
+
 const Products = () => {
   return (
-    <div>Products</div>
-  )
-}
-export default Products
+    <>
+      <Filters />
+      <ProductsContainer />
+      <PaginationContainer />
+    </>
+  );
+};
+export default Products;
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -4,8 +4,7 @@ import { useState } from 'react';
 
 export const loader = async ({ params }) => {
   const response = await customFetch(`/products/${params.id}`);
-  const product = response.data.data;
-  return { product };
+  return { product: response.data.data };
 };
 
 const SingleProduct = () => {
