c96ee14adb44885c7fd038d6725cd4cd51b4e415
miawithcode <cuttontail@gmail.com>
2024-02-14 16:31:49 +0800

    feat: setup redux toolkit and react-hot-toast

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Toaster } from 'react-hot-toast'
+import { store } from './store.js'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+      <Toaster position="top-center" />
+    </Provider>
+  </React.StrictMode>
+);
