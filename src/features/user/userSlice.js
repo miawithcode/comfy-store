12c2ee30975ea095f229a1d4836f5fd7836a84a2
miawithcode <cuttontail@gmail.com>
2024-02-23 20:34:52 +0800

    feat: setup user slice

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,27 @@
+const { createSlice } = require('@reduxjs/toolkit');
+import toast from 'react-hot-toast';
+
+const initialState = {
+  user: { username: 'mia' },
+  theme: cmyk,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    loginUser: (state, action) => {
+      console.log('login');
+    },
+    logoutUser: (state) => {
+      console.log('logout');
+    },
+    toggleTheme: (state) => {
+      console.log('toggle theme');
+    },
+  },
+});
+
+export const { loginUser, logoutUser, toggleTheme } = userSlice.actions;
+
+export default userSlice.reducer;
