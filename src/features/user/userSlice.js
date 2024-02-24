7f54d26d923644e3157e08c118c2b4ce8b6206ff
miawithcode <cuttontail@gmail.com>
2024-02-24 22:35:23 +0800

    feat: login user reducer

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -12,8 +12,12 @@ const getThemeFromLocalStorage = () => {
   return theme;
 };
 
+const getUserFromLocalStorage = () => {
+  return JSON.parse(localStorage.getItem('user')) || null;
+};
+
 const initialState = {
-  user: { username: 'mia' },
+  user: getUserFromLocalStorage(),
   theme: getThemeFromLocalStorage(),
 };
 
@@ -22,12 +26,14 @@ const userSlice = createSlice({
   initialState,
   reducers: {
     loginUser: (state, action) => {
-      console.log(action.payload);
+      const user = { ...action.payload.user, token: action.payload.jwt };
+      state.user = user;
+      localStorage.setItem('user', JSON.stringify(user));
     },
     logoutUser: (state) => {
       state.user = null;
       localStorage.removeItem('user');
-      toast.success('Logged out successfully')
+      toast.success('Logged out successfully');
     },
     toggleTheme: (state) => {
       const { cmyk, night } = themes;
